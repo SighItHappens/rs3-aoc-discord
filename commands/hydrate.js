f05a61688bc985ed4a3e0d55b528e78e987258f0
Sid Suresh <suresh.siddharth@gmail.com>
2020-11-23 00:22:38 -0600

    Completes Aura's changes

--- commands/hydrate.js
+++ commands/hydrate.js
@@ -4,7 +4,16 @@ module.exports = {
     syntax: '!hydrate (#channel-name)',
     displayHelp: true,
     execute(message, Discord, args) {
-        print(message);
-        message.channel.send('Remember to hydrate! <:cup_with_straw:780312397936066600:>');
+        if (args.length != 1) {
+            message.channel.send('Please tag a channel you want to send this message to!');
+            return;
+        }
+
+        let channelName = args[0];
+        channelName = channelName.substr(2, channelName.length - 3);
+        console.log(channelName);
+
+        client.channels.cache.get(channelName).send('Remember to hydrate! :cup_with_straw:');
+
     },
 };
